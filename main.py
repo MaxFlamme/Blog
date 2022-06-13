6d55edf1710962f0293a81f8f2912c8ef1662cc1
Maksym Lukianchuk <maksyml@neogames.com>
2022-06-13 10:54:06 +0300

    Fix sqlalchemy.exc.NoSuchModuleError error

--- main.py
+++ main.py
@@ -28,7 +28,9 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
+if app.config['SQLALCHEMY_DATABASE_URI'].startswith("postgres://"):
+    uri = app.config['SQLALCHEMY_DATABASE_URI'].replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
